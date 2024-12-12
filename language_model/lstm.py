5138aac92b3b9d83db1b01c29653c08d617ea21b
CosteaRazvan <razvancostea99@gmail.com>
2024-12-12 10:25:32 +0200

    LSTM bug

--- language_model/lstm.py
+++ language_model/lstm.py
@@ -8,11 +8,24 @@ class LanguageModel(nn.Module):
             vocab_size = params["vocab_size"]
             embed_dim = params["embed_dim"]
             hidden_dim = params["hidden_dim"]
+            num_layers = params["num_layers"]
             
         self.embedding = nn.Embedding(vocab_size, embed_dim, padding_idx=0)
-        self.lstm = nn.LSTM(embed_dim, hidden_dim, batch_first=True)
+        self.lstm = nn.LSTM(embed_dim, hidden_dim, num_layers=num_layers, batch_first=True)
 
     def forward(self, x):
         x = self.embedding(x)
         _, (hn, _) = self.lstm(x)
-        return hn[0]
+        return hn[-1]
+    
+if __name__ == "__main__":
+    model = LanguageModel({
+        "vocab_size": 3622, 
+        "embed_dim": 256,
+        "hidden_dim": 512
+    }).to("cuda")
+
+    x = torch.randint(3622, size=(3622,)).to("cuda")
+
+    y = model(x)
+
